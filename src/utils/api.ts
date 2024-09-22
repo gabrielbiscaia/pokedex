a2afd6a8dd0ff1368a2be8deb02aa13873010454
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 20:51:59 -0300

    refactor: putting all pokemons of first generation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
-export const getPokemonList = async (limit = 20, offset = 0) => {
+export const getPokemonList = async (limit = 151, offset = 0) => {
   const response = await axios.get(
     `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`,
   );
