58ee54c3c75b7e5b65fcd34ae7ee7c9b7d9ce522
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 20:07:02 -0300

    feat: pokeapi

---
+++ src/utils/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export const getPokemonList = async (limit = 20, offset = 0) => {
+  const response = await axios.get(
+    `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`,
+  );
+  return response.data;
+};
+
+export const getPokemonDetails = async (nameOrId: string | number) => {
+  const response = await axios.get(`${BASE_URL}/pokemon/${nameOrId}`);
+  return response.data;
+};
