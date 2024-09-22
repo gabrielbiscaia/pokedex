d0727aacc8c45a7a96b60a374239349da65cf663
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 20:52:41 -0300

    refactor: description of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pokedex",
-  description: "A Pokedex using Next, Tailwind, and TypeScript",
+  description: "A Pokedex of the First Generation Pokemon",
 };
 
 export default function RootLayout({
