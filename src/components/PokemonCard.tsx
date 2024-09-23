aa8f6d20a8c913fcb58fbf701c16ad9dd186a0d3
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 22:54:46 -0300

    style: changed first letter and color of pokemon types

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-
+import typeColors from "../constants/typeColors";
 import { PokemonDetails } from "../types/pokemon";
 
 interface PokemonCardProps {
@@ -23,13 +23,13 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
       <h2 className="text-xl font-semibold text-center text-black">
         {capitalizeFirstLetter(pokemon.name)}
       </h2>
-      <div className="flex justify-center mt-2">
-        {pokemon.types.map((type) => (
+      <div className="flex justify-center gap-2 mt-2">
+        {pokemon.types.map((type, index) => (
           <span
-            key={type.type.name}
-            className="text-sm bg-gray-200 rounded-full px-4 py-2 mr-2"
+            key={index}
+            className={`text-sm rounded-full px-4 py-2 text-white ${typeColors[type.type.name] || "bg-gray-200 text-black"}`}
           >
-            {type.type.name}
+            {capitalizeFirstLetter(type.type.name)}
           </span>
         ))}
       </div>
