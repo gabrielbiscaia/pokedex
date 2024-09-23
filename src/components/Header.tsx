6b7633a48f8ccbdfe2e10ec89ccf04593ec1c89f
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 22:55:08 -0300

    refactor: refactoring header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React, { useState } from "react";
-import { Settings } from "lucide-react";
+import Image from "next/image";
 import SearchBar from "./SearchBar";
 
 const Header: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const pokeballIco = "/pokeball.svg";
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -18,12 +19,10 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-red-500 p-4">
-      <div className="flex items-center justify-between">
-        <h1 className="text-white text-2xl font-bold">Pokedex</h1>
-        <button className="text-white">
-          <Settings size={24} />
-        </button>
+    <header className="bg-red-500 p-4 rounded-lg border-white border-4 mb-4">
+      <div className="flex items-center justify-center">
+        <Image src={pokeballIco} alt="Pokeball" width={40} height={40} />
+        <h1 className="text-white text-4xl font-bold">Pokédex</h1>
       </div>
       <div className="mt-4">
         <SearchBar
