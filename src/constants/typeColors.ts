6cc1e869e7d3b57d943967bc0054738b4d3ae02c
gabrielbiscaia <biscadev@gmail.com>
2024-09-22 22:51:49 -0300

    feat: added colors to pokemon types

---
+++ src/constants/typeColors.ts
@@ -0,0 +1,22 @@
+const typeColors: { [key: string]: string } = {
+  bug: "bg-[#A8B820]",
+  dark: "bg-[#705848]",
+  dragon: "bg-[#7038F8]",
+  electric: "bg-[#F8D030]",
+  fairy: "bg-[#EE99AC]",
+  fighting: "bg-[#C03028]",
+  fire: "bg-[#F08030]",
+  flying: "bg-[#A890F0]",
+  ghost: "bg-[#705898]",
+  grass: "bg-[#78C850]",
+  ground: "bg-[#E0C068]",
+  ice: "bg-[#98D8D8]",
+  normal: "bg-[#A8A878]",
+  poison: "bg-[#A040A0]",
+  psychic: "bg-[#F85888]",
+  rock: "bg-[#B8A038]",
+  steel: "bg-[#B8B8D0]",
+  water: "bg-[#6890F0]",
+};
+
+export default typeColors;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,28 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    "bg-[#A8B820]",
+    "bg-[#705848]",
+    "bg-[#7038F8]",
+    "bg-[#F8D030]",
+    "bg-[#EE99AC]",
+    "bg-[#C03028]",
+    "bg-[#F08030]",
+    "bg-[#A890F0]",
+    "bg-[#705898]",
+    "bg-[#78C850]",
+    "bg-[#E0C068]",
+    "bg-[#98D8D8]",
+    "bg-[#A8A878]",
+    "bg-[#A040A0]",
+    "bg-[#F85888]",
+    "bg-[#B8A038]",
+    "bg-[#B8B8D0]",
+    "bg-[#6890F0]",
+    "text-white",
+    "text-black",
+  ],
   theme: {
     extend: {
       colors: {
