9c04cf957f703cb624f117203d9a3722fe7df00b
gabrielbiscaia <biscadev@gmail.com>
2024-10-01 12:01:21 -0300

    style: # in front of pokemon number

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -19,7 +19,7 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
     <Link href={`/pokemon/${pokemon.id}`} className="w-full">
       <div className="bg-white shadow-md rounded-xl relative border-2 border-slate-800 hover:border-2 hover:border-red-500">
         <span className="absolute top-2 right-2 text-black px-2 py-1 text-sm font-semibold">
-          {formatPokemonNumber(pokemon.id)}
+          #{formatPokemonNumber(pokemon.id)}
         </span>
         <Image
           src={pokemon.sprites.front_default}
